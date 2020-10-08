530e7b5f747949a209f731d4aa04bc5e3e0bc9a4
Maria Sichka <mariasana9@gmail.com>
2020-10-09 00:52:41 +0300

    Change images.

--- js/main.js
+++ js/main.js
@@ -10,14 +10,17 @@ $(function(){
 $('.heroes__slider-img').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
-  asNavFor: '.heroes__slider-text'
+  asNavFor: '.heroes__slider-text',
+  prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
+  nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>'
 });
 
 $('.heroes__slider-text').slick({
-  slidesToShow: 3,
+  slidesToShow: 1,
   slidesToScroll: 1,
   asNavFor: '.heroes__slider-img',
-  fade: true
+  fade: true,
+  arrows: false
 });
 
 });
