6cf45ee7d6b263b9d7da462e493e8a621df6603e
Maria Sichka <mariasana9@gmail.com>
2020-10-09 21:42:02 +0300

    Add responsive.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 $(function(){
 
+  $('.menu__btn').on('click', function(){
+    $('.menu__list').toggleClass('menu__list--active');
+  });
+
 $('.heroes__slider-img').slick({
   slidesToShow: 1,
   slidesToScroll: 1,
   asNavFor: '.heroes__slider-text',
   prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow-left.png" alt="prev"></button>',
-  nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>'
+  nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow-right.png" alt="next"></button>',
+  responsive: [
+    {
+      breakpoint: 769,
+      settings: {
+        arrows: false      }
+    }
+  ]
 });
 
 $('.heroes__slider-text').slick({
